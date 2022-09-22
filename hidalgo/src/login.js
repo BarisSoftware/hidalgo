9bf5e959b93c538c0964025b0ad8ac5272db299a
imtheswan <juanpamora31@gmail.com>
2022-09-22 10:43:34 -0500

    Agregó estilos de login

--- hidalgo/src/login.js
+++ hidalgo/src/login.js
@@ -1,9 +1,9 @@
+import './login.css';
+
 function InputField(props){
     return(
     <div class = "inputField">
         <label name = {props.name}>
-            {//<hr/>
-            }
             {props.tittle}
             <br/>
             <input type={props.type} placeholder={props.placeh} value={props.value}/>
@@ -13,12 +13,18 @@ function InputField(props){
 
 function Login(props) {
     return(
-    <div>
-        <InputField name = "name" tittle = "Nombre:" type = "text" placeh = "Tu Nombre"/>
-        <InputField name = "lastname" tittle = "Apellido:" type = "text" placeh = "Tu Apellido"/>
-        <InputField name = "pass" tittle = "Contraseña:" type = "password"/>
-        <InputField name = "passconfirmation" tittle = "Confirma Contraseña:" type = "password"/>
-        <InputField name = "submit" type = "submit" value = "Enviar"/>
+    <div id='login'>
+        <form action={props.action} method="GET">
+            <InputField name = "name" tittle = "Nombre:" type = "text" placeh = "Tu Nombre"/>
+            <InputField name = "lastname" tittle = "Apellido:" type = "text" placeh = "Tu Apellido"/>
+            <br/>
+            <InputField name = "pass" tittle = "Contraseña:" type = "password"/>
+            <InputField name = "passconfirmation" tittle = "Confirma Contraseña:" type = "password"/>
+            <br/>
+            <InputField name = "tecnologias" tittle = "Tecnologías conocidas" type = "text" placeh = "ReactJS, NodeJS"/>
+            <br/>
+            <InputField name = "submit" type = "submit" value = "Enviar"/>
+        </form>
     </div>);
 }
 
