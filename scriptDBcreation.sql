aae6445d5e334c50126cb7d69a96625344ec87ae
imtheswan <juanpamora31@gmail.com>
2022-10-13 07:32:31 -0500

    El script de bases de datos funciona, es necesario crear vistas y demas herramientas que ayuden a mantener el orden de los datos, ej: insertar datos de direccion, perfil y llaves automaticamente despues de la creacion de algun proyecto

--- scriptDBcreation.sql
+++ scriptDBcreation.sql
@@ -1,4 +1,4 @@
-drop database kanoa;
+drop database if exists kanoa;
 
 create database kanoa;
 
@@ -17,6 +17,17 @@ create table Proyectos(
     descripcion varchar(45) not null
 );
 
+create table Direccion(
+idDireccion int primary key auto_increment,
+idProyecto int not null,
+sitioweb varchar(60),
+archivos longblob,
+version varchar(10),
+archivosHash binary(8),
+malwareStrikes int,
+foreign key (idProyecto) references Proyectos(idProyecto) on update cascade on delete cascade
+);
+
 create table Perfiles(
 	idPerfil int primary key auto_increment,
     tipoPerfil int not null,
@@ -29,10 +40,36 @@ create table Llaves(
     idLlave int primary key auto_increment,
     tipoLlave int not null,
     propietarioLLave int not null,
-    llave binary(3072)
+    llave binary(255)
 );
 
-create table Creaciones(
+create table Creadores(
     idCreacion int  primary key auto_increment,
-    foreign key idUsuario 
+    idUsuario int not null,
+    idProyecto int not null,
+    foreign key (idUsuario) references Usuarios(idUsuario) on update cascade on delete cascade,
+    foreign key (idProyecto) references Proyectos(idProyecto) on update cascade on delete cascade
+);
+
+create table Administradores(
+	idAdministracion int primary key auto_increment,
+    idUsuario int not null,
+    idProyecto int not null,
+    foreign key (idUsuario) references Usuarios(idUsuario) on update cascade on delete cascade,
+    foreign key (idProyecto) references Proyectos(idProyecto) on update cascade on delete cascade
+);
+
+create table Colaboradores(
+	idAdministracion int primary key auto_increment,
+    idUsuario int not null,
+    idProyecto int not null,
+    foreign key (idUsuario) references Usuarios(idUsuario) on update cascade on delete cascade,
+    foreign key (idProyecto) references Proyectos(idProyecto) on update cascade on delete cascade
+);
+create table Seguidores(
+	idAdministracion int primary key auto_increment,
+    idUsuario int not null,
+    idProyecto int not null,
+    foreign key (idUsuario) references Usuarios(idUsuario) on update cascade on delete cascade,
+    foreign key (idProyecto) references Proyectos(idProyecto) on update cascade on delete cascade
 );
