414a7523b03aa6705715bf3401c5428cec8ab4ef
AdrianPZG <perez.galicia.adrian589@gmail.com>
2022-11-15 21:47:39 -0600

    Primera Parte

---
+++ kanoa-baris/src/HeaderHome.js
@@ -0,0 +1,19 @@
+export const HeaderHome = () => {
+  return (
+    <header>
+        <div class="header">
+            <a href="#" class="logo">Baris <br></br>Software Development</a>
+            <nav>
+                <li class="normal"><a href="cproyecto.html">CREAR PROYECTO</a></li>
+                <li class="normal"><a href="transacciones.html">TRANSACCIONES </a></li>
+                <li class="normal"><a href="sc.html">Source Code</a></li>
+                <li class="cta"><a href="Login.html">LOGIN</a></li>
+            </nav>
+        </div>
+    </header>
+
+    // <nav>
+    //     <Link to="/logins">Logins</Link>
+    // </nav>
+  )
+}
