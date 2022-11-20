cb6c933211a2fbc8fc382641ef932bb9c9ae55cc
imtheswan <juanpamora31@gmail.com>
2022-11-19 21:29:38 -0600

    Partial Fix

--- kanoa-baris/src/App.js
+++ kanoa-baris/src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-<<<<<<< HEAD
-import Home from "./Home"
-=======
 import Administrar from "./AdministarPerfil";
 import Crear from "./CrearProyecto";
 import Home from "./Home"
 import Homefeed from "./Homefeed";
->>>>>>> ReactAppJS-hotfix
 import Login from "./Login"
 import Registro from "./Registro"
 
@@ -20,12 +16,9 @@ function App() {
     <Route exact path="/" element={<Home />} />
     <Route exact path="/login" element={<Login />} />
     <Route exact path="/registro" element={<Registro />} />
-<<<<<<< HEAD
-=======
     <Route exact path="/admin" element={<Administrar />} />
     <Route exact path="/crear" element={<Crear />} />
     <Route exact path="/home_feed" element={<Homefeed />} />
->>>>>>> ReactAppJS-hotfix
     </Routes>
 </BrowserRouter>
 
